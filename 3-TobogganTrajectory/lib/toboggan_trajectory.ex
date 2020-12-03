153ac3391cf00321f3821faa05ad62e9fd9e8fa7
David Vrensk <david@vrensk.com>
2020-12-03 12:08:29 +0100

    Solved 3:2

--- 3-TobogganTrajectory/lib/toboggan_trajectory.ex
+++ 3-TobogganTrajectory/lib/toboggan_trajectory.ex
@@ -4,44 +4,75 @@ defmodule TobogganTrajectory do
   """
 
   @doc """
-  Count trees going down.
-
-      iex> forest = TobogganTrajectory.parse_forest(TobogganTrajectory.sample_forest())
-      iex> forest |> TobogganTrajectory.right_3_down_1()
-      7
+  iex> forest = TobogganTrajectory.parse_forest(TobogganTrajectory.sample_forest())
+  iex> TobogganTrajectory.five_slopes_product(forest)
+  336
   """
-  def right_3_down_1({_trees, max_row, _max_col} = forest) do
-    0..max_row
-    |> Enum.count(fn r -> tree?(forest, {r, r * 3}) end)
+  def five_slopes_product(forest) do
+    five_slopes(forest)
+    |> Enum.reduce(1, fn count, acc -> count * acc end)
   end
 
   @doc """
-  Find a tree in a forest.
-  ## Examples
+  * Right 1, down 1.
+  * Right 3, down 1. (This is the slope you already checked.)
+  * Right 5, down 1.
+  * Right 7, down 1.
+  * Right 1, down 2.
 
-      iex> forest = TobogganTrajectory.parse_forest(TobogganTrajectory.sample_forest())
-      iex> forest |> TobogganTrajectory.tree?({0, 2})
-      true
-      iex> forest |> TobogganTrajectory.tree?({4, 12})
-      true
+  iex> forest = TobogganTrajectory.parse_forest(TobogganTrajectory.sample_forest())
+  iex> TobogganTrajectory.five_slopes(forest)
+  [2, 7, 3, 4, 2]
+  """
+  def five_slopes(forest) do
+    [
+      {1, 1},
+      {3, 1},
+      {5, 1},
+      {7, 1},
+      {1, 2}
+    ]
+    |> Enum.map(fn {n, m} -> TobogganTrajectory.right_n_down_m(forest, n, m) end)
+  end
+
+  @doc """
+  iex> forest = TobogganTrajectory.parse_forest(TobogganTrajectory.sample_forest())
+  iex> forest |> TobogganTrajectory.right_n_down_m(3, 1)
+  7
+  iex> forest |> TobogganTrajectory.right_n_down_m(1, 1)
+  2
+  iex> forest |> TobogganTrajectory.right_n_down_m(1, 2)
+  2
+  iex> forest |> TobogganTrajectory.right_n_down_m(1, 3)
+  0
+  """
+  def right_n_down_m({_trees, max_row, _max_col} = forest, n, m) do
+    0..max_row
+    |> Enum.filter(fn r -> rem(r, m) == 0 end)
+    |> Enum.count(fn r -> tree?(forest, {r, div(r * n, m)}) end)
+  end
+
+  @doc """
+  iex> forest = TobogganTrajectory.parse_forest(TobogganTrajectory.sample_forest())
+  iex> forest |> TobogganTrajectory.tree?({0, 2})
+  true
+  iex> forest |> TobogganTrajectory.tree?({4, 12})
+  true
   """
   def tree?({trees, _max_row, max_col}, {r, c}) do
     trees[{r, rem(c, max_col + 1)}]
   end
 
   @doc """
-  Parse a forest.
-  ## Examples
-
-      iex> {trees, max_row, max_col} = TobogganTrajectory.parse_forest(TobogganTrajectory.sample_forest())
-      iex> max_row
-      10
-      iex> max_col
-      10
-      iex> trees[{0,0}]
-      false
-      iex> trees[{2,6}]
-      true
+  iex> {trees, max_row, max_col} = TobogganTrajectory.parse_forest(TobogganTrajectory.sample_forest())
+  iex> max_row
+  10
+  iex> max_col
+  10
+  iex> trees[{0,0}]
+  false
+  iex> trees[{2,6}]
+  true
   """
   def parse_forest(string) do
     pairs =
--- 3-TobogganTrajectory/test/toboggan_trajectory_test.exs
+++ 3-TobogganTrajectory/test/toboggan_trajectory_test.exs
@@ -5,8 +5,15 @@ defmodule TobogganTrajectoryTest do
   test "puzzle 1: right 3, down 1" do
     File.read!("input.txt")
     |> TobogganTrajectory.parse_forest()
-    |> TobogganTrajectory.right_3_down_1()
+    |> TobogganTrajectory.right_n_down_m(3, 1)
     |> assert_eq(247)
+  end
+
+  test "puzzle 2: 5 slopes" do
+    File.read!("input.txt")
+    |> TobogganTrajectory.parse_forest()
+    |> TobogganTrajectory.five_slopes_product()
+    |> assert_eq(2_983_070_376)
   end
 
   def assert_eq(a, b), do: assert(a == b)
