e55cdbb1bf7224ad38be269c0989152de0e645e3
David Vrensk <david@vrensk.com>
2020-12-05 23:53:01 +0100

    Solved 5:2

--- 5-BinaryBoarding/lib/binary_boarding.ex
+++ 5-BinaryBoarding/lib/binary_boarding.ex
@@ -14,4 +14,11 @@ defmodule BinaryBoarding do
     |> String.replace(~w[F L], "0")
     |> String.to_integer(2)
   end
+
+  @doc """
+  iex> BinaryBoarding.first_gap([3,4,5,7,8])
+  6
+  """
+  def first_gap([a, b | _]) when a + 2 == b, do: a + 1
+  def first_gap([_ | rest]), do: first_gap(rest)
 end
--- 5-BinaryBoarding/test/binary_boarding_test.exs
+++ 5-BinaryBoarding/test/binary_boarding_test.exs
@@ -9,5 +9,15 @@ defmodule BinaryBoardingTest do
     |> Enum.max(fn -> 0 end)
     |> assert_eq(965)
   end
+
+  test "puzzle 2: my seat in file" do
+    File.read!("input.txt")
+    |> String.split()
+    |> Enum.map(&BinaryBoarding.seat_id/1)
+    |> Enum.sort()
+    |> BinaryBoarding.first_gap()
+    |> assert_eq(524)
+  end
+
   def assert_eq(a, b), do: assert(a == b)
 end
