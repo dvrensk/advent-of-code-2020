612d44b914734f0c9b582eb2e9f8bb147b1e4e1b
David Vrensk <david@vrensk.com>
2020-12-06 00:07:38 +0100

    extract funs for reuse

--- 5-BinaryBoarding/test/binary_boarding_test.exs
+++ 5-BinaryBoarding/test/binary_boarding_test.exs
@@ -3,21 +3,23 @@ defmodule BinaryBoardingTest do
   doctest BinaryBoarding
 
   test "puzzle 1: max seat id in file" do
-    File.read!("input.txt")
-    |> String.split()
+    input_lines()
     |> Enum.map(&BinaryBoarding.seat_id/1)
     |> Enum.max(fn -> 0 end)
     |> assert_eq(965)
   end
 
   test "puzzle 2: my seat in file" do
-    File.read!("input.txt")
-    |> String.split()
+    input_lines()
     |> Enum.map(&BinaryBoarding.seat_id/1)
     |> Enum.sort()
     |> BinaryBoarding.first_gap()
     |> assert_eq(524)
   end
 
+  def input_lines(path \\ "input.txt"),
+    do: File.read!(path) |> String.split("\n", trim: true)
+  def ints_from_file(path \\ "input.txt"),
+    do: File.read!(path) |> String.split() |> Enum.map(&String.to_integer/1)
   def assert_eq(a, b), do: assert(a == b)
 end
