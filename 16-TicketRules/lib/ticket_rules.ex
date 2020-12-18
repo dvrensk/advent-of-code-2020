82c73dad3c3779f9d2430620ae6ecdde2d326063
David Vrensk <david@vrensk.com>
2020-12-18 13:12:38 +0100

    Solved 16:2

--- 16-TicketRules/lib/ticket_rules.ex
+++ 16-TicketRules/lib/ticket_rules.ex
@@ -1,7 +1,84 @@
 defmodule TicketRules do
+  def puzzle2(input) do
+    names =
+      input
+      |> myopic_possibilities()
+      |> restricted_possibilities()
+
+    %{mine: mine} = parse(input)
+    List.zip([names, mine])
+    |> Enum.filter(&match?({"departure"<>_, _}, &1))
+    |> Enum.map(&elem(&1,1))
+    |> Enum.reduce(fn n, acc -> acc * n end)
+  end
+  @doc """
+  iex> ([["row"], ["class", "row"], ["class", "row", "seat"]]
+  ...> |> TicketRules.restricted_possibilities())
+  ~w[row class seat]
+  """
+  def restricted_possibilities(myopic) do
+    if restricted?(myopic) do
+      myopic |> List.flatten()
+    else
+      singles = singles_in(myopic)
+
+      myopic
+      |> Enum.map(fn
+        [a] -> [a]
+        longer -> Enum.reject(longer, &Enum.member?(singles, &1))
+      end)
+      |> restricted_possibilities()
+    end
+  end
+
+  def restricted?(myopic),
+    do:
+      Enum.all?(myopic, fn
+        [_] -> true
+        _ -> false
+      end)
+
+  def singles_in(list_of_lists),
+    do: list_of_lists |> Enum.filter(&match?([_], &1)) |> List.flatten()
+
   @doc """
   iex> import TicketRules
-  iex> error_rate(sample())
+  iex> myopic_possibilities(sample(2))
+  [["row"], ["class", "row"], ["class", "row", "seat"]]
+  """
+  def myopic_possibilities(input) do
+    %{rules: rules, mine: mine, nearby: nearby} = parse(input)
+    good = nearby |> Enum.filter(fn t -> invalid_values(t, rules) == [] end)
+
+    [mine | good]
+    |> Enum.map(fn ticket -> ticket |> Enum.map(&compatibility(&1, rules)) end)
+    |> transpose()
+    |> Enum.map(&reduce_to_common/1)
+  end
+
+  def compatibility(n, rules) do
+    rules
+    |> Enum.filter(fn {_name, [r1, r2]} -> Enum.member?(r1, n) or Enum.member?(r2, n) end)
+    |> Enum.map(&elem(&1, 0))
+  end
+
+  def transpose(rows), do: rows |> List.zip() |> Enum.map(&Tuple.to_list/1)
+
+  @doc """
+  iex> ([["class", "row", "seat"], ["row", "seat"], ["class", "row"], ["class", "row", "seat"]]
+  ...> |> TicketRules.reduce_to_common())
+  ["row"]
+  """
+  def reduce_to_common(list_of_lists) do
+    list_of_lists
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.reduce(&MapSet.intersection/2)
+    |> MapSet.to_list()
+  end
+
+  @doc """
+  iex> import TicketRules
+  iex> error_rate(sample(1))
   71
   """
   def error_rate(input) do
@@ -31,10 +108,14 @@ defmodule TicketRules do
 
   @doc """
   iex> import TicketRules
-  iex> parse(sample())
+  iex> parse(sample(1))
   %{rules: %{"class" => [1..3, 5..7], "row" => [6..11, 33..44], "seat" => [13..40, 45..50]},
     mine: [7,1,14],
     nearby: [[7,3,47], [40,4,50], [55,2,20], [38,6,12]]}
+  iex> parse(sample(2))
+  %{rules: %{"class" => [0..1, 4..19], "row" => [0..5, 8..19], "seat" => [0..13, 16..19]},
+    mine: [11,12,13],
+    nearby: [[3,9,18], [15,1,5], [5,14,9]]}
   """
   def parse(input) do
     [a, "your ticket:\n" <> b, "nearby tickets:\n" <> c] = String.split(input, "\n\n", trim: true)
@@ -54,14 +135,14 @@ defmodule TicketRules do
   end
 
   def parse_rule(str) do
-    [name | nums] = Regex.run(~r/(\w+): (\d+)-(\d+) or (\d+)-(\d+)/, str, capture: :all_but_first)
+    [name | nums] = Regex.run(~r/([^:]+): (\d+)-(\d+) or (\d+)-(\d+)/, str, capture: :all_but_first)
     [a1, a2, b1, b2] = Enum.map(nums, &String.to_integer/1)
     {name, [a1..a2, b1..b2]}
   end
 
   def parse_ticket(str), do: str |> String.split(",") |> Enum.map(&String.to_integer/1)
 
-  def sample() do
+  def sample(1) do
     """
     class: 1-3 or 5-7
     row: 6-11 or 33-44
@@ -75,6 +156,22 @@ defmodule TicketRules do
     40,4,50
     55,2,20
     38,6,12
+    """
+  end
+
+  def sample(2) do
+    """
+    class: 0-1 or 4-19
+    row: 0-5 or 8-19
+    seat: 0-13 or 16-19
+
+    your ticket:
+    11,12,13
+
+    nearby tickets:
+    3,9,18
+    15,1,5
+    5,14,9
     """
   end
 end
--- 16-TicketRules/test/ticket_rules_test.exs
+++ 16-TicketRules/test/ticket_rules_test.exs
@@ -6,6 +6,17 @@ defmodule TicketRulesTest do
     assert TicketRules.error_rate(input()) == 22977
   end
 
+  # test "names in 2" do
+  #   input()
+  #   |> TicketRules.myopic_possibilities()
+  #   |> TicketRules.restricted_possibilities()
+  #   |> assert_eq([])
+  # end
+
+  test "puzzle 2: departure checksum" do
+    assert TicketRules.puzzle2(input()) == 998_358_379_943
+  end
+
   def input(path \\ "input.txt"), do: File.read!(path)
   def input_lines(path \\ "input.txt"), do: input(path) |> String.split("\n", trim: true)
   def input_paragraphs(path \\ "input.txt"), do: input(path) |> String.split("\n\n", trim: true)
