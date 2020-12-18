10b6420ef831b6d6a457613a75833cd306c272fe
David Vrensk <david@vrensk.com>
2020-12-18 02:34:50 +0100

    Solved 15:2

--- 15-Numbers/lib/numbers.ex
+++ 15-Numbers/lib/numbers.ex
@@ -1,42 +1,45 @@
 defmodule Numbers do
   @doc """
-  iex> Numbers.spoken_2020("0,3,6")
+  iex> Numbers.spoken("0,3,6", 2020)
   436
   """
-  def spoken_2020(input) do
-    start(input)
-    |> run_until(2020)
-    |> hd()
-    |> elem(0)
-  end
+  def spoken(input, limit), do: start(input) |> run_until(limit)
 
-  def run_until([{_, x} | _] = list, x), do: list
-  def run_until(list, x), do: next(list) |> run_until(x)
+  def run_until({ix, prev, _}, ix), do: prev
+  def run_until(list, ix), do: next(list) |> run_until(ix)
 
   @doc """
-  iex> Numbers.next([{6,3}, {3,2}, {0, 1}])
-  [{0,4}, {6,3}, {3,2}, {0, 1}]
-  iex> Numbers.next([{0,4}, {6,3}, {3,2}, {0, 1}])
-  [{3,5}, {0,4}, {6,3}, {3,2}, {0, 1}]
-  iex> Numbers.next([{3,5}, {0,4}, {6,3}, {3,2}, {0, 1}])
-  [{3,6}, {3,5}, {0,4}, {6,3}, {3,2}, {0, 1}]
+  iex> res = Numbers.next({3, 6, %{0 => [1], 3 => [2], 6 => [3]}})
+  {4, 0, %{0 => [4,1], 3 => [2], 6 => [3]}}
+  iex> res = Numbers.next(res)
+  {5, 3, %{0 => [4,1], 3 => [5,2], 6 => [3]}}
+  iex> Numbers.next(res)
+  {6, 3, %{0 => [4,1], 3 => [6,5,2], 6 => [3]}}
   """
-  def next([{spoken, ix} | mem] = all_mem) do
-    Enum.find(mem, fn {x, _} -> x == spoken end)
-    |> case do
-      nil -> [{0, ix + 1} | all_mem]
-      {_, pos} -> [{ix - pos, ix + 1} | all_mem]
+  def next({ix, prev, mem}) do
+    case mem[prev] do
+      [_] -> {ix + 1, 0, add(mem, 0, ix + 1)}
+      [b, a | _] -> {ix + 1, b - a, add(mem, b - a, ix + 1)}
     end
   end
 
   @doc """
   iex> Numbers.start("0,3,6")
-  [{6,3}, {3,2}, {0, 1}]
+  {3, 6, %{0 => [1], 3 => [2], 6 => [3]}}
   """
   def start(input) do
-    input
-    |> String.split(",", trim: true)
-    |> Enum.reduce({1, []}, fn x, {ix, mem} -> {ix + 1, [{String.to_integer(x), ix} | mem]} end)
-    |> elem(1)
+    numbers =
+      input
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    mem =
+      numbers
+      |> Enum.with_index(1)
+      |> Enum.reduce(%{}, fn {n, ix}, mem -> add(mem, n, ix) end)
+
+    {length(numbers), hd(Enum.reverse(numbers)), mem}
   end
+
+  def add(mem, n, ix), do: Map.update(mem, n, [ix], fn list -> [ix | list] end)
 end
--- 15-Numbers/test/numbers_test.exs
+++ 15-Numbers/test/numbers_test.exs
@@ -3,7 +3,11 @@ defmodule NumbersTest do
   doctest Numbers
 
   test "puzzle 1" do
-    assert Numbers.spoken_2020("17,1,3,16,19,0") == 694
+    assert Numbers.spoken("17,1,3,16,19,0", 2020) == 694
+  end
+
+  test "puzzle 2" do
+    assert Numbers.spoken("17,1,3,16,19,0", 30_000_000) == 21_768_614
   end
 
   def assert_eq(a, b), do: assert(a == b)
